922c37ba7a1ba45e1a2d31f2498101054a8037b3
Diego Peñaflor <dpenaflor98@gmail.com>
2022-03-24 18:01:27 -0500

    Añnadidos folder y ejerciciosñ

---
+++ Tarea Punteros/ejercicio1.cpp
@@ -0,0 +1,23 @@
+// Ejercicio 1 Punteros
+
+#include "iostream"
+#include "stdio.h"
+
+using namespace std;
+
+int main()
+{
+    int a; //Ingresar Valor de A
+    cout << "Ingrese Valor de a: ";
+    cin >> a;
+
+    int * ptr = &a; //Guarda la direccion de a
+
+    cout << "Valor de a: " << a << "\n";
+    cout << "Valor de &a (direccion): " << ptr << "\n";
+    cout << "Valor de &ptr (direccion): " << &ptr << "\n";
+
+    system("pause");
+
+    return 0;
+}
---
+++ Tarea Punteros/ejercicio2.cpp
@@ -0,0 +1,35 @@
+// Ejercicio 2 Punteros
+
+#include "iostream"
+#include "stdio.h"
+
+using namespace std;
+
+int main()
+{
+    //Codigo 1
+    /*int* ptrVar;
+    *ptrVar = 10;
+    cout<<*ptrVar;*/
+
+    // Este codigo  crea un puntero, pero no define a donde apunta, luego procede a darle un valor
+    // a esa direccion desconocida, lo que provoca un error.
+
+    //Codigo 2
+    /*int*p, n = 15;
+    *p = n;
+    cout<<*p<< "\n";*/
+
+    // Este codigo funciona perfectamente (faltaba una p)
+
+    // Codigo 3
+    int* array = new int[2];
+    *array = 10;
+    *(array + 1) = 15;
+    array[2] = 20; 
+
+
+    system("pause");
+
+    return 0;
+}
