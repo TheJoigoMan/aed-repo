ed2fcf11bdd6d21a0620a1b85b5d1ed15b68c93c
TheJoigoMan <dpenaflor98@gmail.com>
2023-04-11 16:39:12 -0500

    Añadida Tarea de Forward List

--- Sparse_Matrix/sparsem.h
+++ Sparse_Matrix/sparsem.h
@@ -13,34 +13,136 @@ class SparseMatrix {
                 int pos_col;
                 Node* next_row;
                 Node* next_col;
+
+                Node()
+                {
+                    pos_col = pos_row = -1;
+                    next_col = next_row = nullptr;
+                }
+
+                Node(int i, int j, T value)
+                {
+                    pos_row = i;
+                    pos_col = j;
+                    data = value;
+                    next_col = next_row = nullptr;
+                }
         };
 
         vector<Node*> rows;
         vector<Node*> cols;
         int n_cols;
         int n_rows;
-   public:
-      SparseMatrix(int nc, int nr) {
-          //TODO: inicializar rows y cols en nullptr
-      }     
+    public:
+        SparseMatrix(int nr, int nc) 
+        {
+            n_cols = nc;
+            n_rows = nr;
+            
+            for(int c = 0; c < nc; c++)
+                cols.push_back(nullptr);
+            
+            for(int r = 0; r < nr; r++)
+                rows.push_back(nullptr);
+        }     
 
-      void set(int i, int j, T value){
-          //TODO
-          if(i < 0 || i >= n_rows || j < 0 || j >= n_cols) return;
-          Node* head_row = this->rows[i];
-          
-          
-      }
+        void set(int i, int j, T value)
+        {
+            if(i < 0 || i >= n_rows || j < 0 || j >= n_cols) return;
+            Node* nuevo = new Node(i, j, value);   
 
-      void display_byrows(){
-          //mostrar por fila
-      }
+            // 1- trabajo en filas    
+            Node* ant = getPrevCol(i, j);
+            if(ant == nullptr){
+                nuevo->next_row = rows[i];
+                rows[i] = nuevo;
+            }
+            else if(ant->pos_col == j)
+            {
+                ant->data = value;
+                //delete nuevo;
+            }
+            else
+            {
+                nuevo->next_row = ant->next_row;
+                ant->next_row = nuevo;
+            }
+   
+            // 1- trabajo en columnas
+            Node* ant_r = getPrevRow(i, j);
+            if(ant_r == nullptr){
+                nuevo->next_col = cols[j];
+                cols[j] = nuevo;
+            }
+            else if(ant_r->pos_row == i)
+            {
+                ant_r->data = value;
+                //delete nuevo;
+            }
+            else
+            {
+                nuevo->next_col = ant_r->next_col;
+                ant_r->next_col = nuevo;
+            }
+        }
 
-      void display_bycols(){
-          //mostrar por fila
-      }  
+        void display_byrows()
+        {
+            for(int i=0;i<n_rows;i++)
+            {
+                cout<<i<<":: ";
+                Node* temp = rows[i];
+                while(temp != nullptr) 
+                {
+                    cout<<"("<<temp->pos_row<<","<<temp->pos_col<<","<<temp->data<<") ->";
+                    temp = temp->next_row;
+                }
+                cout<<endl;
+            }
+        }
 
-      ~SparseMatrix(){
-          //TODO
-      }
+        void display_bycols(){
+            for(int i=0; i<n_cols; i++)
+                cout << i << "      ";
+            cout << endl;
+            
+            for(int i=0; i<n_cols; i++)
+            {
+                cout << i << ":: ";
+                Node* temp = cols[i];
+                while(temp != nullptr)
+                {
+                    cout << "("<<temp->pos_row<<","<<temp->pos_col<<","<<temp->data<<") ->";
+                    temp = temp->next_col;
+                }
+                cout << endl;
+            }
+        }  
+
+    private:
+        Node *getPrevCol(int i, int j)
+        {
+            Node *ant = nullptr;
+            Node *temp = rows[i];
+
+            while( temp != nullptr && j > temp->pos_col )
+            {
+                ant = temp;
+                temp = temp->next_row;
+            }
+            return ant;
+        }
+
+        Node *getPrevRow(int i, int j)
+        {
+            Node *ant = nullptr;
+            Node *temp = cols[j];
+
+            while( temp != nullptr && i > temp->pos_row )
+            {
+                ant = temp;
+                temp = temp->next_col;
+            }
+            return ant;
+        }
 };
---
+++ Tarea Forward List/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "forward_list.h"
+using namespace std;
+
+void testIntersect(){
+    Node* lista1 = nullptr;
+    push_front(lista1, 64);
+    push_front(lista1, 32);
+    push_front(lista1, 16);
+    push_front(lista1, 8);
+    push_front(lista1, 4);
+    push_front(lista1, 2);
+    Display(lista1); // Revisar lista1
+
+    Node* lista2 = nullptr;
+    push_back(lista2, 16);
+    push_back(lista2, 32);
+    push_back(lista2, 64);
+    push_back(lista2, 128);
+    push_back(lista2, 256);
+    Display(lista2); // Revisar lista2
+
+    Node* lista3 = Intersect(lista1, lista2); // O(n)
+    Display(lista3); // 16 -> 32 -> 64 -> NULL
+}
+
+int main() 
+{
+    cout << "Intersect Lists O(n): \n";
+    testIntersect();
+
+    return 0;
+}
